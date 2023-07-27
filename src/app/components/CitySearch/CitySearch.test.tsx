12a94ec66e94d13968e27718918baa9e7776ecb8
Romulo Mont Serrat <romulo.serrat@barkyn.com>
2023-07-27 12:50:58 -0300

    remove fireEvent for test

--- src/app/components/CitySearch/CitySearch.test.tsx
+++ src/app/components/CitySearch/CitySearch.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CitySearch from './CitySearch';
 import axios from 'axios';
