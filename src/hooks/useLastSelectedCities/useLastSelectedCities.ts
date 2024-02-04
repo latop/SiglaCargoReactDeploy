15d8b1582c5a2ba5c8d829755c6dd975b4e9bf7b
Romulo Mont Serrat <romulo.serrat@barkyn.com>
2024-02-04 13:37:44 -0300

    update keys

--- src/hooks/useLastSelectedCities/useLastSelectedCities.ts
+++ src/hooks/useLastSelectedCities/useLastSelectedCities.ts
@@ -5,7 +5,7 @@ import { recoilPersist } from "recoil-persist";
 const { persistAtom } = recoilPersist();
 
 export const lastSelectedCitiesState = atom<ICity[]>({
-  key: "lastlastSelectedCitiesState",
+  key: "lastSelectedCitiesState",
   default: [],
   effects_UNSTABLE: [persistAtom],
 });
