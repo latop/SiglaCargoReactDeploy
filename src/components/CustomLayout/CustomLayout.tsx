c4591d62c96f98097e2a0c85b1fdb55f6427aa50
Romulo Mont Serrat <romulo.serrat@barkyn.com>
2024-02-16 12:19:01 -0300

    fix ts

--- src/components/CustomLayout/CustomLayout.tsx
+++ src/components/CustomLayout/CustomLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
 
-export function CustomLayout({ children }) {
+export function CustomLayout({ children }: { children: React.ReactNode }) {
   return (
     <Box sx={{ display: "flex", flexDirection: "column" }}>{children}</Box>
   );
