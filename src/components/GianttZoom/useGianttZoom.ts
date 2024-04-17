267f1c620151db0c5f4526a15b68cb7d4518c2b0
Romulo Mont Serrat <romulo.serrat@barkyn.com>
2024-04-17 16:59:30 -0300

    fix zoom

--- src/components/GianttZoom/useGianttZoom.ts
+++ src/components/GianttZoom/useGianttZoom.ts
@@ -10,24 +10,23 @@ enum Zoom {
   FourteenDays = "14",
 }
 
-const setEndDate = (startDate: string | null, zoom: Zoom) => {
+const setEndDate = (startDate: Date | null, zoom: Zoom) => {
   if (Number(zoom) < 1) {
     return dayjs(startDate)
       .add(Number(zoom) * 24, "hour")
       .toDate();
   }
-  return dayjs(startDate).add(2, "hour").add(Number(zoom), "day").toDate();
+  return dayjs(startDate).add(Number(zoom), "day").toDate();
 };
 
-const setStartDate = (startDate: string | null) => {
-  return dayjs(startDate).add(-2, "hour").toDate();
+const setStartDate = (startDate: Date | null) => {
+  return dayjs(startDate).toDate();
 };
 
 export function useGianttZoom() {
   const [currentZoom, setCurrentZoom] = React.useState<Zoom>(Zoom.SevenDays);
   const {
     visibleTimeEnd,
-    startDate,
     visibleTimeStart,
     setVisibleTimeEnd,
     setVisibleTimeStart,
@@ -39,8 +38,8 @@ export function useGianttZoom() {
   ) => {
     if (newZoom) {
       setCurrentZoom(newZoom);
-      setVisibleTimeStart(setStartDate(startDate));
-      setVisibleTimeEnd(setEndDate(startDate, newZoom as Zoom));
+      setVisibleTimeStart(setStartDate(visibleTimeStart));
+      setVisibleTimeEnd(setEndDate(visibleTimeStart, newZoom as Zoom));
     }
   };
 
