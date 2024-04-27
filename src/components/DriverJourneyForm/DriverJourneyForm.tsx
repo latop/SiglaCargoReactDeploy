63177af300d6637203baa7c14adb621b527f41a0
Romulo Mont Serrat <romulo.serrat@barkyn.com>
2024-04-27 20:42:08 -0300

    line code in demand

--- src/components/DriverJourneyForm/DriverJourneyForm.tsx
+++ src/components/DriverJourneyForm/DriverJourneyForm.tsx
@@ -64,7 +64,7 @@ export const DriverJourneyForm = ({
       tasksDriver.push({
         seq: tasksDriver.length + 1,
         type: "V",
-        demand: data.sto,
+        demand: data.lineCode,
         locOrig: data.locationOrigCode,
         locDest: data.locationDestCode,
         lineCode: data.lineCode,
