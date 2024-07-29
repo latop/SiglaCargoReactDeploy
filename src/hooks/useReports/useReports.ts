1b2f112ef12d5e2332342ffa26ec49617d38920c
Erik <ol.erik0107@gmail.com>
2024-07-29 19:05:12 +0200

    feat: criando body para recuperar report

--- src/hooks/useReports/useReports.ts
+++ src/hooks/useReports/useReports.ts
@@ -1,7 +1,9 @@
 import { ReportsResponse } from "@/interfaces/reports";
 import { fetchReports } from "@/services/reports";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import useSWR, { SWRConfiguration } from "swr";
+import { useFetch } from "../useFetch";
+import { useToast } from "../useToast";
 
 export const useReports = (options?: SWRConfiguration) => {
   const { data, error, isLoading, mutate } = useSWR<ReportsResponse[]>(
@@ -9,11 +11,28 @@ export const useReports = (options?: SWRConfiguration) => {
     fetchReports,
     { ...options },
   );
-
+  const { addToast } = useToast();
+  const [postReports] = useFetch();
   const methods = useForm();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const onSubmit = (data: any) => {
-    console.log(data);
+  const onSubmit = async (data: FieldValues) => {
+    const parameter = Object.entries(data)
+      .map(([key, value]) => {
+        if (key.includes("reportCode")) return;
+        return value;
+      })
+      .filter((value) => value != undefined || value != null || value == "");
+    const body = {
+      reportCode: data.reportCode,
+      parameter,
+    };
+    await postReports("/api/Report/Report", body, {
+      onSuccess: () => {
+        addToast("Relatório criado com sucesso", { type: "success" });
+      },
+      onError: () => {
+        addToast("Erro ao criar relatório", { type: "error" });
+      },
+    });
   };
 
   return {
