2026f387ff89877f3cb218628146d4582c550a0b
Gustavo Rocha <drbrucce@gmail.com>
2024-10-23 11:56:21 -0300

    feat: add react query

--- src/providers.tsx
+++ src/providers.tsx
@@ -7,18 +7,21 @@ import theme from "@/styles/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import { ToastProvider } from "@/hooks/useToast/useToast";
 import { DialogProvider } from "./hooks/useDialog/useDialog";
+import QueryProvider from "./services/query/provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <ToastProvider>
-          <DialogProvider>
-            <GlobalStyles />
-            {children}
-          </DialogProvider>
-        </ToastProvider>
-      </ThemeProvider>
+      <QueryProvider>
+        <ThemeProvider theme={theme}>
+          <ToastProvider>
+            <DialogProvider>
+              <GlobalStyles />
+              {children}
+            </DialogProvider>
+          </ToastProvider>
+        </ThemeProvider>
+      </QueryProvider>
     </RecoilRoot>
   );
 }
---
+++ src/services/query/provider.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+interface Params {
+  children: React.ReactNode;
+}
+
+export const queryClient = new QueryClient();
+const QueryProvider = ({ children }: Params) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
