21b83211e15788364264fd3ba55fa0e98b1e40dd
Erik <ol.erik0107@gmail.com>
2024-11-29 17:38:28 -0300

    fix: format cellphone func

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,9 +11,9 @@ export const generateRandomColor = () => {
 export const formatCellphone = (value: string) => {
   return value
     .replace(/\D/g, "")
-    .replace(/(\d{2})(\d)/, "($1) $2")
-    .replace(/(\d{5})(\d{1,4})/, "$1-$2")
-    .slice(0, 15);
+    .replace(/(\d{2})(\d{1})/, "($1) $2")
+    .replace(/(\d{1})(\d{4})(\d{1,4})/, "$1 $2-$3")
+    .slice(0, 16);
 };
 
 export const formatCep = (value: string) => {
