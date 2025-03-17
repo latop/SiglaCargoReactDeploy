c1f0296cda79680c65e874883bb42281f091d77a
Erik <ol.erik0107@gmail.com>
2025-03-17 15:16:45 -0300

    fix: remove unecessary code

--- src/components/ImportTripsDialog/useImportTripsDialog.ts
+++ src/components/ImportTripsDialog/useImportTripsDialog.ts
@@ -4,27 +4,6 @@ import { useHash } from "@/hooks/useHash";
 import { fetchGtmById } from "@/services/import-trips";
 import React from "react";
 import useSWR from "swr";
-import { z } from "zod";
-
-const schema = z.object({
-  code: z.string(),
-  codeIntegration1: z.string().optional(), //gps
-  codeIntegration2: z.string().optional(), // TMS
-  name: z.string(),
-  cityId: z.string().optional(),
-  city: z.record(z.any()).optional(),
-  latitude: z.coerce.number(),
-  longitude: z.coerce.number(),
-  locationGroup: z.record(z.any()).optional(),
-  locationGroupId: z.string().optional().nullable(),
-  locationTypeId: z.string(),
-  locationType: z.record(z.any()).optional(),
-  timezoneId: z.string().optional(),
-  startDate: z.string().optional(),
-  endDate: z.string().optional(),
-});
-
-export type LocationFormType = z.infer<typeof schema>;
 
 export const useImportTripsDialog = () => {
   const [hash] = useHash();
