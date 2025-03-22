c5da7da1c05248c2e0b86a184e65aa1ecb2be2ce
Erik <ol.erik0107@gmail.com>
2025-03-22 18:38:18 -0300

    feat: split files for menu config

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -12,28 +12,10 @@ import {
   styled,
 } from "@mui/material";
 import Link from "next/link";
-import PersonSearchIcon from "@mui/icons-material/PersonSearch";
-import HomeIcon from "@mui/icons-material/Home";
-import { TbSteeringWheel } from "react-icons/tb";
-import LocalShippingIcon from "@mui/icons-material/LocalShipping";
-import SwapVertIcon from "@mui/icons-material/SwapVert";
-import MovingIcon from "@mui/icons-material/Moving";
-import { BiTrip } from "react-icons/bi";
-import NoCrashIcon from "@mui/icons-material/NoCrash";
-import AssignmentIcon from "@mui/icons-material/Assignment";
-import UploadFileIcon from "@mui/icons-material/UploadFile";
-import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest";
-import RouteIcon from "@mui/icons-material/Route";
-import PublishIcon from "@mui/icons-material/Publish";
-import AddLocationAltIcon from "@mui/icons-material/AddLocationAlt";
 import { useRouter } from "next/navigation";
 import { BurgerMenuGroup } from "../BurgerMenuGroup";
-import EditNoteIcon from "@mui/icons-material/EditNote";
-import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
-import AltRouteIcon from "@mui/icons-material/AltRoute";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import AutoGraphIcon from "@mui/icons-material/AutoGraph";
-import BarChartIcon from "@mui/icons-material/BarChart";
+
+import { groupments, routes } from "./config";
 
 const ButtonStyled = styled(Button)`
   transition: all 0.2s ease-in-out;
@@ -75,149 +57,6 @@ export interface RouteItem {
   path: string;
   group?: GroupmentType;
 }
-
-const groupConfigs = [
-  {
-    groupment: "register",
-    name: "Cadastros",
-    icon: PlaylistAddIcon,
-  },
-  {
-    groupment: "coordination",
-    name: "Coordenação de Viagens",
-    icon: AltRouteIcon,
-  },
-  {
-    groupment: "driver-schedule",
-    name: "Escala de Motoristas",
-    icon: CalendarMonthIcon,
-  },
-  {
-    groupment: "planning",
-    name: "Planejamento",
-    icon: AutoGraphIcon,
-  },
-  {
-    groupment: "reports",
-    name: "Relatórios",
-    icon: BarChartIcon,
-  },
-] satisfies Array<{
-  groupment: GroupmentType;
-  name: string;
-  icon: React.ElementType;
-}>;
-
-const routes: RouteItem[] = [
-  { text: "Home", icon: <HomeIcon />, path: "/home" },
-  { text: "Rotas", icon: <RouteIcon />, path: "/lines", group: "register" },
-  {
-    text: "Motoristas",
-    icon: <TbSteeringWheel />,
-    path: "/drivers",
-    group: "register",
-  },
-  {
-    text: "Caminhão",
-    icon: <LocalShippingIcon />,
-    path: "/trucks",
-    group: "register",
-  },
-  {
-    text: "Localização",
-    icon: <AddLocationAltIcon />,
-    path: "/locations",
-    group: "register",
-  },
-  {
-    text: "Setor Responsável",
-    icon: <PersonSearchIcon />,
-    path: "/responsible-sector",
-    group: "register",
-  },
-  {
-    text: "Justificativas",
-    icon: <EditNoteIcon />,
-    path: "/justifications",
-    group: "register",
-  },
-
-  // Coordenação de Viagens
-  {
-    text: "Coordenação de viagens",
-    icon: <TbSteeringWheel />,
-    path: "/daily-trips-schedule",
-    group: "coordination",
-  },
-  {
-    text: "Partidas e chegadas",
-    icon: <SwapVertIcon />,
-    path: "/departures-and-arrivals",
-    group: "coordination",
-  },
-  {
-    text: "Viagens diárias",
-    icon: <MovingIcon />,
-    path: "/daily-trips",
-    group: "coordination",
-  },
-
-  // Escala de Motoristas
-  {
-    text: "Solicitação de Motoristas",
-    icon: <LocalShippingIcon />,
-    path: "/drivers-request",
-    group: "driver-schedule",
-  },
-  {
-    text: "Escala de Motoristas",
-    icon: <LocalShippingIcon />,
-    path: "/drivers-schedule",
-    group: "driver-schedule",
-  },
-  {
-    text: "Liberação de motoristas",
-    icon: <NoCrashIcon />,
-    path: "/release-driver",
-    group: "driver-schedule",
-  },
-  {
-    text: "Publicação",
-    icon: <PublishIcon />,
-    path: "/publish-journey",
-    group: "driver-schedule",
-  },
-
-  // Planejamento
-  {
-    text: "Planejamento de veículos",
-    icon: <TbSteeringWheel />,
-    path: "/vehicle-planning",
-    group: "planning",
-  },
-  { text: "Cenários", icon: <BiTrip />, path: "/scenarios", group: "planning" },
-  {
-    text: "Importação de viagens",
-    icon: <UploadFileIcon />,
-    path: "/import-trips",
-    group: "planning",
-  },
-  {
-    text: "Otimizacão de viagens",
-    icon: <SettingsSuggestIcon />,
-    path: "/trip-optimization",
-    group: "planning",
-  },
-
-  // Relatórios
-  {
-    text: "Relatórios",
-    icon: <AssignmentIcon />,
-    path: "/reports",
-    group: "reports",
-  },
-];
-
 const STORAGE_KEY = "burgerMenuOpenGroups";
 
 const initialState: Record<GroupmentType, boolean> = {
@@ -241,10 +80,10 @@ const saveOpenState = (state: Record<GroupmentType, boolean>) => {
 export function BurgerMenu({ isOpen, toggleDrawer }: BurgerMenuProps) {
   const router = useRouter();
 
-  const [open, setOpen] = useState(initialState);
+  const [menuOpen, setOpenMenu] = useState(initialState);
 
   const handleMenuOpen = ({ group }: { group: GroupmentType }) => {
-    setOpen((prev) => {
+    setOpenMenu((prev) => {
       const newState = { ...prev, [group]: !prev[group] };
       saveOpenState(newState);
       return newState;
@@ -252,13 +91,13 @@ export function BurgerMenu({ isOpen, toggleDrawer }: BurgerMenuProps) {
   };
 
   const handleMenuCloseAll = () => {
-    setOpen(initialState);
+    setOpenMenu(initialState);
     saveOpenState(initialState);
   };
 
   useEffect(() => {
     const stored = getInitialOpenState();
-    setOpen(stored);
+    setOpenMenu(stored);
   }, []);
 
   return (
@@ -317,7 +156,7 @@ export function BurgerMenu({ isOpen, toggleDrawer }: BurgerMenuProps) {
                   </ListItem>
                 ))}
 
-              {groupConfigs.map(({ groupment, name, icon: Icon }) => (
+              {groupments.map(({ groupment, name, icon: Icon }) => (
                 <BurgerMenuGroup
                   key={groupment}
                   routes={routes}
@@ -325,7 +164,7 @@ export function BurgerMenu({ isOpen, toggleDrawer }: BurgerMenuProps) {
                   name={name}
                   router={router}
                   icon={Icon}
-                  expanded={open[groupment]}
+                  expanded={menuOpen[groupment]}
                   onToggle={() => handleMenuOpen({ group: groupment })}
                 />
               ))}
