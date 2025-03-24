9b9c7b1160214f4d010a210eee029c93c9a0cebd
Erik <ol.erik0107@gmail.com>
2025-03-24 12:20:30 -0300

    fix: align color and btn

--- src/features/ActivityType/columnsConfig.tsx
+++ src/features/ActivityType/columnsConfig.tsx
@@ -69,12 +69,18 @@ export const columnsConfig = ({
         return (
           <div
             style={{
-              width: "25px",
-              height: "25px",
-              background: `${params.row.color}`,
-              borderRadius: "50%",
+              paddingTop: 6,
             }}
-          />
+          >
+            <div
+              style={{
+                width: "25px",
+                height: "25px",
+                background: `${params.row.color}`,
+                borderRadius: "50%",
+              }}
+            />
+          </div>
         );
       },
     },
@@ -87,7 +93,7 @@ export const columnsConfig = ({
           <button
             disabled={isLoadingDelete}
             style={{
-              paddingTop: 12,
+              paddingTop: 6,
               display: "flex",
               gap: "8px",
               border: "none",
