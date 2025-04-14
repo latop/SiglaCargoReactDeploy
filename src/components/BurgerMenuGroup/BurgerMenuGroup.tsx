1ec8d4a170b1fad4bb168e1807517ae24ea3c64f
Erik <ol.erik0107@gmail.com>
2025-04-14 20:40:21 -0300

    feat: menu item idetification

--- src/components/BurgerMenuGroup/BurgerMenuGroup.tsx
+++ src/components/BurgerMenuGroup/BurgerMenuGroup.tsx
@@ -16,6 +16,8 @@ import MenuIcon from "@mui/icons-material/Menu";
 import debounce from "debounce";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 import { GroupmentType, RouteItem } from "../BurgerMenu/config";
+import { usePathname } from "next/navigation";
+import { blue } from "@mui/material/colors";
 
 type BurgerMenuGroupProps = {
   routes: RouteItem[];
@@ -50,6 +52,12 @@ export const BurgerMenuGroup = ({
   expanded,
   onToggle,
 }: BurgerMenuGroupProps) => {
+  const pathname = usePathname();
+  const isActive = (path: string) => pathname === path;
+  const hasActiveItem = routes
+    .filter(({ group }) => group === groupment)
+    .some(({ path }) => isActive(path));
+
   return (
     <AccordionStyled
       expanded={expanded}
@@ -57,11 +65,27 @@ export const BurgerMenuGroup = ({
       disableGutters
       square
     >
-      <AccordionSummaryStyled expandIcon={<ExpandMoreIcon />}>
-        <ListItemIcon>
+      <AccordionSummaryStyled
+        expandIcon={<ExpandMoreIcon />}
+        sx={{
+          borderLeft: hasActiveItem ? `4px solid ${blue[800]}` : "none",
+          color: hasActiveItem ? blue[800] : "inherit",
+          "& .MuiAccordionSummary-expandIconWrapper": {
+            color: hasActiveItem ? blue[900] : "inherit",
+            fontWeight: hasActiveItem ? 500 : 400,
+          },
+        }}
+      >
+        <ListItemIcon sx={{ color: hasActiveItem ? blue[800] : "inherit" }}>
           <Icon />
         </ListItemIcon>
-        <Typography fontWeight={500}>{name}</Typography>
+        <Typography
+          sx={{
+            color: hasActiveItem ? blue[900] : "inherit",
+          }}
+        >
+          {name}
+        </Typography>
       </AccordionSummaryStyled>
       <AccordionDetails>
         {routes
@@ -77,9 +101,29 @@ export const BurgerMenuGroup = ({
                   debounce(() => router.prefetch(path), 300)();
                 }}
               >
-                <ListItemButton>
-                  <ListItemIcon>{icon}</ListItemIcon>
-                  <ListItemText primary={text} />
+                <ListItemButton
+                  sx={{
+                    color: isActive(path) ? blue[800] : "inherit",
+                    borderLeft: isActive(path)
+                      ? `4px solid ${blue[800]}`
+                      : "none",
+                    pl: isActive(path) ? "12px" : "16px",
+                  }}
+                >
+                  <ListItemIcon
+                    sx={{ color: isActive(path) ? blue[800] : "inherit" }}
+                  >
+                    {icon}
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={text}
+                    sx={{
+                      "& .MuiTypography-root": {
+                        fontWeight: isActive(path) ? 500 : 400,
+                        color: isActive(path) ? blue[900] : "inherit",
+                      },
+                    }}
+                  />
                 </ListItemButton>
               </Link>
             </ListItem>
