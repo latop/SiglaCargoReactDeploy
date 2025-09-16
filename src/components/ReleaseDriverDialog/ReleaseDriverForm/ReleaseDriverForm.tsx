ee36b5a7730714a8753bee64885643d81fcc7942
Erik <ol.erik0107@gmail.com>
2025-09-16 17:17:18 -0300

    feat: adding new labels to dialog

--- src/components/ReleaseDriverDialog/ReleaseDriverForm/ReleaseDriverForm.tsx
+++ src/components/ReleaseDriverDialog/ReleaseDriverForm/ReleaseDriverForm.tsx
@@ -1,15 +1,5 @@
-import {
-  Box,
-  Grid,
-  MenuItem,
-  TextField,
-  Typography,
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-} from "@mui/material";
+import { Box, Grid, MenuItem, TextField, Typography } from "@mui/material";
 import { Controller, useFormContext } from "react-hook-form";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import { AutocompleteDriver } from "@/components/AutocompleteDriver";
 import { AutocompleteTruck } from "@/components/AutocompleteTruck";
@@ -213,252 +203,244 @@ export const ReleaseDriverForm = () => {
               </Grid>
             </Grid>
           </Box>
-          <Accordion>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-              <Typography fontWeight={500}>Mais informações</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Grid container gap={1}>
-                <Box sx={{ flexBasis: "135px" }}>
-                  <Controller
-                    name="mdfe"
-                    control={control}
-                    render={({ field }) => {
-                      return (
-                        <TextField
-                          sx={{
-                            "& .MuiInputBase-input.Mui-disabled": {
-                              WebkitTextFillColor: "#000000",
-                              opacity: 1,
-                            },
-                          }}
-                          label="MDFE"
-                          type="tel"
-                          {...field}
-                          value={field.value}
-                          onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            e.target.value = e.target.value
-                              .slice(0, 12)
-                              .replace(/\D/g, "");
-                          }}
-                          inputProps={{
-                            maxLength: 12,
-                          }}
-                        />
-                      );
-                    }}
-                  />
-                </Box>
-                <Box sx={{ flexBasis: "135px" }}>
-                  <Controller
-                    name="cte"
-                    control={control}
-                    render={({ field }) => {
-                      return (
-                        <TextField
-                          sx={{
-                            "& .MuiInputBase-input.Mui-disabled": {
-                              WebkitTextFillColor: "#000000",
-                              opacity: 1,
-                            },
-                          }}
-                          label="CTE"
-                          {...field}
-                          type="tel"
-                          value={field.value}
-                          onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            e.target.value = e.target.value
-                              .slice(0, 10)
-                              .replace(/\D/g, "");
-                          }}
-                          inputProps={{
-                            maxLength: 10,
-                          }}
-                        />
-                      );
-                    }}
-                  />
-                </Box>
-                <Box sx={{ flexBasis: "135px" }}>
-                  <Controller
-                    name="palletInvoice"
-                    control={control}
-                    render={({ field }) => {
-                      return (
-                        <TextField
-                          label="Nota do Pallet"
-                          {...field}
-                          type="tel"
-                          value={field.value}
-                          inputProps={{
-                            maxLength: 10,
-                          }}
-                        />
-                      );
-                    }}
-                  />
-                </Box>
-                <Box sx={{ flexBasis: "135px" }}>
-                  <Controller
-                    name="productInvoice"
-                    control={control}
-                    render={({ field }) => {
-                      return (
-                        <TextField
-                          label="Nota do produto"
-                          {...field}
-                          type="tel"
-                          value={field.value}
-                          inputProps={{
-                            maxLength: 10,
-                          }}
-                        />
-                      );
-                    }}
-                  />
-                </Box>
-                <Box sx={{ flexBasis: "165px" }}>
-                  <Controller
-                    name={"isReturnLoaded"}
-                    control={control}
-                    render={({ field }) => (
-                      <TextField
-                        label="Retorno Carregado?"
-                        select
-                        sx={{
-                          width: "100%",
-                        }}
-                        defaultValue={"false"}
-                        {...field}
-                        onChange={(value) => {
-                          methods.setValue(
-                            "isReturnLoaded",
-                            value?.target.value,
-                          );
-                        }}
-                      >
-                        <MenuItem value={"false"}>Não</MenuItem>
-                        <MenuItem value={"true"}>Sim</MenuItem>
-                      </TextField>
-                    )}
-                  />
-                </Box>
-                <Box sx={{ flexBasis: "135px" }}>
-                  <Controller
-                    name="licensePlateTrailer"
-                    control={control}
-                    render={({ field, fieldState }) => {
-                      return (
-                        <TextField
-                          required={false}
-                          error={fieldState.error ? true : false}
-                          sx={{
-                            width: "100%",
-                            "& .MuiInputBase-input.Mui-disabled": {
-                              WebkitTextFillColor: "#000000",
-                              opacity: 1,
-                            },
-                          }}
-                          label="Placa da Carreta"
-                          {...field}
-                          value={field.value}
-                          inputProps={{ maxLength: 50 }}
-                          onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            e.target.value = e.target.value
-                              .replace(/[^A-Za-z0-9]/, "")
-                              .toUpperCase();
-                            const matchs = Array.from(
-                              e.target.value.matchAll(
-                                /([A-z]{3})(\d)([A-j0-9])(\d{2})/g,
-                              ),
-                            );
-                            const partials = [];
-                            if (matchs.length > 0) {
-                              partials.push(matchs[0][1]);
-                              if (!isNaN(Number.parseInt(matchs[0][3]))) {
-                                partials.push("-");
-                              }
-                              partials.push(matchs[0][2]);
-                              partials.push(matchs[0][3]);
-                              partials.push(matchs[0][4]);
-                              e.target.value = partials.join("").toUpperCase();
-                            }
-                          }}
-                        />
-                      );
-                    }}
-                  />
-                </Box>
-                <Box sx={{ flexBasis: "385px" }}>
-                  <Controller
-                    name="note"
-                    control={control}
-                    render={({ field }) => {
-                      return (
-                        <TextField
-                          sx={{
-                            width: "100%",
-                            "& .MuiInputBase-input.Mui-disabled": {
-                              WebkitTextFillColor: "#000000",
-                              opacity: 1,
-                            },
-                          }}
-                          label="Observações"
-                          {...field}
-                          value={field.value}
-                          inputProps={{ maxLength: 50 }}
-                          onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            e.target.value = e.target.value.slice(0, 50);
-                          }}
-                        />
-                      );
-                    }}
-                  />
-                </Box>
 
-                <Box sx={{ flexBasis: "200px" }}>
-                  <Controller
-                    name="issueDate"
-                    control={control}
-                    render={({ field }) => {
-                      return (
-                        <DateTimePicker
-                          label="Data da emissão da NF"
-                          {...field}
-                          value={field.value ? dayjs(field.value) : null}
-                          onChange={(newValue) => {
-                            field.onChange(newValue);
-                          }}
-                        />
-                      );
+          <Typography fontWeight={500}>Mais informações</Typography>
+          <Grid container gap={1}>
+            <Box sx={{ flexBasis: "135px" }}>
+              <Controller
+                name="mdfe"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <TextField
+                      sx={{
+                        "& .MuiInputBase-input.Mui-disabled": {
+                          WebkitTextFillColor: "#000000",
+                          opacity: 1,
+                        },
+                      }}
+                      label="MDFE"
+                      type="tel"
+                      {...field}
+                      value={field.value}
+                      onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        e.target.value = e.target.value
+                          .slice(0, 12)
+                          .replace(/\D/g, "");
+                      }}
+                      inputProps={{
+                        maxLength: 12,
+                      }}
+                    />
+                  );
+                }}
+              />
+            </Box>
+            <Box sx={{ flexBasis: "135px" }}>
+              <Controller
+                name="cte"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <TextField
+                      sx={{
+                        "& .MuiInputBase-input.Mui-disabled": {
+                          WebkitTextFillColor: "#000000",
+                          opacity: 1,
+                        },
+                      }}
+                      label="CTE"
+                      {...field}
+                      type="tel"
+                      value={field.value}
+                      onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        e.target.value = e.target.value
+                          .slice(0, 10)
+                          .replace(/\D/g, "");
+                      }}
+                      inputProps={{
+                        maxLength: 10,
+                      }}
+                    />
+                  );
+                }}
+              />
+            </Box>
+            <Box sx={{ flexBasis: "135px" }}>
+              <Controller
+                name="palletInvoice"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <TextField
+                      label="Nota do Pallet"
+                      {...field}
+                      type="tel"
+                      value={field.value}
+                      inputProps={{
+                        maxLength: 10,
+                      }}
+                    />
+                  );
+                }}
+              />
+            </Box>
+            <Box sx={{ flexBasis: "135px" }}>
+              <Controller
+                name="productInvoice"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <TextField
+                      label="NF Pallet"
+                      {...field}
+                      type="tel"
+                      value={field.value}
+                      inputProps={{
+                        maxLength: 10,
+                      }}
+                    />
+                  );
+                }}
+              />
+            </Box>
+            <Box sx={{ flexBasis: "165px" }}>
+              <Controller
+                name={"isReturnLoaded"}
+                control={control}
+                render={({ field }) => (
+                  <TextField
+                    label="Saída Carrega"
+                    select
+                    sx={{
+                      width: "100%",
                     }}
-                  />
-                </Box>
-                <Box sx={{ flexBasis: "280px" }}>
-                  <Controller
-                    name="issueResponsible"
-                    control={control}
-                    render={({ field }) => {
-                      return (
-                        <TextField
-                          label="Responsável pela emissão da NF"
-                          {...field}
-                          sx={{
-                            width: "100%",
-                          }}
-                          type="tel"
-                          value={field.value}
-                          inputProps={{
-                            maxLength: 200,
-                          }}
-                        />
-                      );
+                    defaultValue={"false"}
+                    {...field}
+                    onChange={(value) => {
+                      methods.setValue("isReturnLoaded", value?.target.value);
                     }}
-                  />
-                </Box>
-              </Grid>
-            </AccordionDetails>
-          </Accordion>
+                  >
+                    <MenuItem value={"false"}>Não</MenuItem>
+                    <MenuItem value={"true"}>Sim</MenuItem>
+                  </TextField>
+                )}
+              />
+            </Box>
+            <Box sx={{ flexBasis: "135px" }}>
+              <Controller
+                name="licensePlateTrailer"
+                control={control}
+                render={({ field, fieldState }) => {
+                  return (
+                    <TextField
+                      required={false}
+                      error={fieldState.error ? true : false}
+                      sx={{
+                        width: "100%",
+                        "& .MuiInputBase-input.Mui-disabled": {
+                          WebkitTextFillColor: "#000000",
+                          opacity: 1,
+                        },
+                      }}
+                      label="Placa da Carreta"
+                      {...field}
+                      value={field.value}
+                      inputProps={{ maxLength: 50 }}
+                      onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        e.target.value = e.target.value
+                          .replace(/[^A-Za-z0-9]/, "")
+                          .toUpperCase();
+                        const matchs = Array.from(
+                          e.target.value.matchAll(
+                            /([A-z]{3})(\d)([A-j0-9])(\d{2})/g,
+                          ),
+                        );
+                        const partials = [];
+                        if (matchs.length > 0) {
+                          partials.push(matchs[0][1]);
+                          if (!isNaN(Number.parseInt(matchs[0][3]))) {
+                            partials.push("-");
+                          }
+                          partials.push(matchs[0][2]);
+                          partials.push(matchs[0][3]);
+                          partials.push(matchs[0][4]);
+                          e.target.value = partials.join("").toUpperCase();
+                        }
+                      }}
+                    />
+                  );
+                }}
+              />
+            </Box>
+            <Box sx={{ flexBasis: "385px" }}>
+              <Controller
+                name="note"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <TextField
+                      sx={{
+                        width: "100%",
+                        "& .MuiInputBase-input.Mui-disabled": {
+                          WebkitTextFillColor: "#000000",
+                          opacity: 1,
+                        },
+                      }}
+                      label="Observações"
+                      {...field}
+                      value={field.value}
+                      inputProps={{ maxLength: 50 }}
+                      onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        e.target.value = e.target.value.slice(0, 50);
+                      }}
+                    />
+                  );
+                }}
+              />
+            </Box>
+
+            <Box sx={{ flexBasis: "200px" }}>
+              <Controller
+                name="issueDate"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <DateTimePicker
+                      label="Data de Emissão MDFE"
+                      {...field}
+                      value={field.value ? dayjs(field.value) : null}
+                      onChange={(newValue) => {
+                        field.onChange(newValue);
+                      }}
+                    />
+                  );
+                }}
+              />
+            </Box>
+            <Box sx={{ flexBasis: "280px" }}>
+              <Controller
+                name="issueResponsible"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <TextField
+                      label="Responsável pela emissão MDFE"
+                      {...field}
+                      sx={{
+                        width: "100%",
+                      }}
+                      type="tel"
+                      value={field.value}
+                      inputProps={{
+                        maxLength: 200,
+                      }}
+                    />
+                  );
+                }}
+              />
+            </Box>
+          </Grid>
         </Box>
       </Box>
       <Box gap="10px" display="flex" flexDirection="column"></Box>
