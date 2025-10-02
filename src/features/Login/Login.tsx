3216881cb0617b3e26d5815075906304853da3d7
Ana Clara Horta <anaclara.horta@latop.com.br>
2025-10-02 19:29:24 -0300

    Update Login.tsx

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -56,7 +56,7 @@ export function Login() {
 
   const handleSSO = () => {
     const targetUrl =
-      "https://apicargodev.azurewebsites.net/Auth/Login?returnUrl=https%3A%2F%2Fhomologpepsicobrasil.sigla.app%2Fhome";
+      "https://apicargo.azurewebsites.net/Auth/Login?returnUrl=https%3A%2F%2Fpepsicobrasil.sigla.app%2Fhome";
     window.location.href = targetUrl;
   };
 
